7cd146f659e66205f45d1cdd539c0ff207021a1b
Luiz G. Salvador <luizg.salvadoroliveira@gmail.com>
2026-09-23 22:07:30 -0300

    Adicionar arquivos de projeto.

---
+++ csharp-wpf-conversor-moedas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csharp_wpf_conversor_moedas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
